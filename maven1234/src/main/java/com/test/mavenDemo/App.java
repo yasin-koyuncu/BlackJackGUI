f850e02f5090b66440a12dd5663245836e3e8bc1
yasin-koyuncu <yasin.koyuncu@hotmail.se>
2016-10-11 13:24:04 +0200

    commit demo

---
+++ maven1234/src/main/java/com/test/mavenDemo/App.java
@@ -0,0 +1,13 @@
+package com.test.mavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
